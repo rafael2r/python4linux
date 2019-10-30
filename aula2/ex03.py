e93e671a8a60096ac013e265110d3da96871b23b
rafael2r <49322855+rafael2r@users.noreply.github.com>
2019-10-29 22:13:36 -0300

    Add files via upload

---
+++ aula2/ex03.py
@@ -0,0 +1,43 @@
+#exemplo de lista
+'''
+lista = [13,15,17,19,21]
+soma = 0
+for numero in lista:
+    soma = soma + numero
+print(soma)
+
+
+lista = []
+soma = 0
+for numero in range(0,5):
+    lista.append(int(input('Digite o valor')))
+    soma = soma + numero
+print(soma)
+
+lista = []
+count = 0
+
+while count <5:
+    lista.append{10}
+    count += 1
+ 
+
+
+contador = 0
+soma = 0
+while contador <10:
+    soma
+'''
+    
+dicionario = {
+    'nota1':5.5,
+    'nota2':6.5,
+    'nota3':7.0,
+    'nota4':3.0
+}
+soma =0
+for valor in dicionario.values():
+    soma += valor
+soma = soma / 4
+print(soma)
+
