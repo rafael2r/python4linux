3e265240bab5700decdbf3e4adfa822d4b88253e
rafael2r <49322855+rafael2r@users.noreply.github.com>
2019-10-29 22:15:02 -0300

    Add files via upload

--- aula2/exemplos.py
+++ aula2/exemplos.py
@@ -1,3 +1,14 @@
 # exemplo de dicionario
 dicionario = {'nome' : 'Rafael', 'idade' : '37'}
 print(type(dicionario))
+
+
+count = 0
+soma = 0
+num[]
+# criar uma lista de 15 numeros e apresentar a soma deles
+
+
+git add aula2/
+git commit -m "aula2 de loop"
+git push
